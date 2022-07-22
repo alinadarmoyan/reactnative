b0c021a5689c7704ec9374736164878e430107b7
gridavid <davit.grigoryan13.y@tumo.org>
2022-07-22 13:08:30 +0400

    Tab navigation icon

--- App.js
+++ App.js
@@ -6,43 +6,61 @@ import HomeScreen from './Components/HomeScreen/HomeScreen';
 import SettingsScreen from './Components/SettingsScreen/SettingsScreen';
 import React, { useEffect, useState } from 'react';
 import api from './Components/helpers/api';
+import { Entypo } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const [countries,setCountries]=useState([]);
-  const getCountries = async()=>{
-    try{
-      const {data} = await api.get('countries.json');
+  const [countries, setCountries] = useState([]);
+  const getCountries = async () => {
+    try {
+      const { data } = await api.get('countries.json');
       setCountries(data);
     }
-    catch(error){
+    catch (error) {
       console.log(error);
-      
+
     }
   }
-  useEffect( ()=>{
+  useEffect(() => {
     getCountries();
 
-  },[]);
+  }, []);
   return (
     <>
       {
-      countries.map(({name, url},index)=>{
-        return(
-          // <Post name={post.title} year={post.body} key={post.id} titleHender={titleHendler}/>
-          <HomeScreen name={name} url={url} key={index} />
-        )
-      })
+        countries.map(({ name, url }, index) => {
+          return (
+            // <Post name={post.title} year={post.body} key={post.id} titleHender={titleHendler}/>
+            <HomeScreen name={name} url={url} key={index} />
+          )
+        })
       }
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Settings" component={SettingsScreen} />
+          <Tab.Screen name="Home" component={HomeScreen} options={{
+            title: 'Home',
+            tabBarIcon: () => {
+              return (
+                <Entypo name="home" size={24} color="black" />
+              );
+            },
+          }} />
+          
+          <Tab.Screen name="Settings" component={SettingsScreen} options={{
+            title: 'Settings',
+            tabBarIcon: () => {
+              return (
+                <Ionicons name="settings" size={24} color="black" />
+              );
+            },
+            tabBarActiveTintColor: 'tomato'
+          }} />
         </Tab.Navigator>
       </NavigationContainer>
     </>
-    
+
   );
 }
 
