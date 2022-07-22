a7d0b14b3c78a2ed9616e0120293bbece1553598
gridavid <davit.grigoryan13.y@tumo.org>
2022-07-22 12:48:02 +0400

    api

--- App.js
+++ App.js
@@ -3,18 +3,46 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './Components/HomeScreen/HomeScreen';
-import SettingsScreen from './Components/SettingsScreen/SettingsScreen'
+import SettingsScreen from './Components/SettingsScreen/SettingsScreen';
+import React, { useEffect, useState } from 'react';
+import api from './Components/helpers/api';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const [countries,setCountries]=useState([]);
+  const getCountries = async()=>{
+    try{
+      const {data} = await api.get('countries.json');
+      setCountries(data);
+    }
+    catch(error){
+      console.log(error);
+      
+    }
+  }
+  useEffect( ()=>{
+    getCountries();
+
+  },[]);
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <>
+      {
+      countries.map(({name, url},index)=>{
+        return(
+          // <Post name={post.title} year={post.body} key={post.id} titleHender={titleHendler}/>
+          <HomeScreen name={name} url={url} key={index} />
+        )
+      })
+      }
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
+    
   );
 }
 
--- Components/HomeScreen/HomeScreen.js
+++ Components/HomeScreen/HomeScreen.js
@@ -1,9 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
-export default function HomeScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
-      </View>
-    );
+export default function HomeScreen({name, url}) {
+  
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {/* <Text>Home!</Text> */}
+      <Text style={{margin: 10}}>{name}</Text>
+      <Text style={{margin: 20}}>{url}</Text>
+    </View>
+  );
 }
