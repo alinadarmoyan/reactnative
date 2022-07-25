f28284b3305cf419d8e8b9acccdd2af3aa4b9f71
user <andranik.d4.y@tumo.org>
2022-07-25 12:25:51 +0400

    Changed Home Page

--- App.js
+++ App.js
@@ -5,38 +5,16 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './Components/HomeScreen/HomeScreen';
 import SettingsScreen from './Components/SettingsScreen/SettingsScreen';
 import React, { useEffect, useState } from 'react';
-import api from './Components/helpers/api';
+// import api from './helpers/api';
 import { Entypo } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const [countries, setCountries] = useState([]);
-  const getCountries = async () => {
-    try {
-      const { data } = await api.get('countries.json');
-      setCountries(data);
-    }
-    catch (error) {
-      console.log(error);
 
-    }
-  }
-  useEffect(() => {
-    getCountries();
-
-  }, []);
   return (
-    <>
-      {
-        countries.map(({ name, url }, index) => {
-          return (
-            // <Post name={post.title} year={post.body} key={post.id} titleHender={titleHendler}/>
-            <HomeScreen name={name} url={url} key={index} />
-          )
-        })
-      }
+
       <NavigationContainer>
         <Tab.Navigator>
           <Tab.Screen name="Home" component={HomeScreen} options={{
@@ -59,7 +37,6 @@ export default function App() {
           }} />
         </Tab.Navigator>
       </NavigationContainer>
-    </>
 
   );
 }
--- Components/HomeScreen/HomeScreen.js
+++ Components/HomeScreen/HomeScreen.js
@@ -1,13 +1,39 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
+import api from '../helpers/api'
+
 
 export default function HomeScreen({name, url}) {
+
+  const [countries, setCountries] = useState([]);
+  const getCountries = async () => {
+    try {
+      const { data } = await api.get('countries.json');
+      setCountries(data);
+    }
+    catch (error) {
+      console.log(error);
+
+    }
+  }
+  useEffect(() => {
+    getCountries();
+
+  }, []);
   
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       {/* <Text>Home!</Text> */}
-      <Text style={{margin: 10}}>{name}</Text>
-      <Text style={{margin: 20}}>{url}</Text>
+      {
+        countries.map(({ name, url }, index) => {
+          return(
+            <View>
+              <Text style={{margin: 10}}>{name}</Text>
+              <Text style={{margin: 20}}>{url}</Text>
+            </View>
+          )
+        })
+      }
     </View>
   );
 }
